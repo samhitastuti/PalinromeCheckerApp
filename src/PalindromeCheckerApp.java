28e384cb62bcc3c68eda2088d3f2ef96cbc2d563
samhitastuti <samhitastuti06@gmail.com>
2026-02-25 15:06:20 +0530

    Usecase 2 is done.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,21 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version:1.0");
-    }
+    public static void main(String[] args) {
 
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+    }
 }
